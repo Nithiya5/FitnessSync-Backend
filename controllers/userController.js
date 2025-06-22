383c019b8e1f00a980257992c541e40a3ca23726
Nithiya5 <123157180+Nithiya5@users.noreply.github.com>
2025-06-22 18:40:32 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -92,7 +92,7 @@ const login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET || 'your_jwt_secret',
-      { expiresIn: '7d' }
+      { expiresIn: '1hr' }
     );
 
     res.status(200).json({
